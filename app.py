6d961dc8830c699dca9f395bce5cde01cb606230
Dominic Cave <dominic@dhjc.uk>
2024-10-11 12:01:40 +0100

    migrate to fasthtml and add behavior

---
+++ app.py
@@ -0,0 +1,15 @@
+from fasthtml.common import *
+
+app,rt = fast_app(pico=False,
+                  hdrs=(
+                      Link(rel='stylesheet', href='style.css', type='text/css'),
+                  ))
+
+@rt('/')
+def get(): return Div(P('Hello World!'), cls="circle", hx_get="/change")
+
+
+@rt('/change')
+def get(): return P('Please do not press this button again')
+
+serve()
